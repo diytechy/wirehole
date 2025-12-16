e069715638068c1115b5fed588fcb8de973952d5
Peter Johnson <peterandersjohnson@outlook.com>
2025-12-16 10:18:11 -0600

    Adding env reset content.

--- ResetEnvFile.bat
+++ ResetEnvFile.bat
@@ -10,3 +10,6 @@ REM Run the PowerShell script with the same name
 powershell -NoProfile -ExecutionPolicy Bypass -File "%scriptPath%%scriptName%.ps1"
 
 endlocal
+
+
+pause
--- ResetEnvFile.ps1
+++ ResetEnvFile.ps1
@@ -1,23 +1,10 @@
 # Get the directory where this script is located
 $scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+$EnvFile = Join-Path $scriptDir ".env"
+$ExampleFile = Join-Path $scriptDir ".env.example"
 
-# List of folders to reset
-$folders = @(
-    "config",
-    "db",
-    "etc-dnsmasq.d",
-    "etc-pihole",
-    "unbound"
-)
-
-foreach ($folder in $folders) {
-    $path = Join-Path $scriptDir $folder
-
-    # Remove the folder if it exists
-    if (Test-Path $path) {
-        Remove-Item -Path $path -Recurse -Force
-    }
-
-    # Create the folder
-    New-Item -Path $path -ItemType Directory | Out-Null
-}
+ if (Test-Path $EnvFile) {
+    if ((Get-FileHash $EnvFile).Hash -ne (Get-FileHash $ExampleFile).Hash) { Read-Host "Files differ. Press Enter to continue" }
+     Remove-Item $EnvFile -Force
+ }
+Copy-Item -Path $ExampleFile -Destination $EnvFile
