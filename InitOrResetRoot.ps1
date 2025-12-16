da541c8a1055af6f31f20cbd0e200c40779af394
Peter Johnson <peterandersjohnson@outlook.com>
2025-12-16 10:09:23 -0600

    Adding reset templates

---
+++ InitOrResetRoot.ps1
@@ -0,0 +1,25 @@
+# Get the directory where this script is located
+$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+# List of folders to reset
+$folders = @(
+    "config",
+    "db",
+    "etc-dnsmasq.d",
+    "etc-pihole",
+    "unbound"
+)
+
+foreach ($folder in $folders) {
+    $path = Join-Path $scriptDir $folder
+
+    # Remove the folder if it exists
+    if (Test-Path $path) {
+        Remove-Item -Path $path -Recurse -Force
+    }
+
+    # Create the folder
+    New-Item -Path $path -ItemType Directory | Out-Null
+}
+$ResetPath = Join-Path $scriptDir "Reset"
+Copy-Item -Path $ResetPath -Destination $scriptDir -Recurse -Force
