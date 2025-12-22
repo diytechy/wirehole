35b875e3ce62acac640ef36aa00c8ba4c8998e56
Peter Johnson <peterandersjohnson@outlook.com>
2025-12-22 11:12:39 -0600

    Rename, adjustments

--- SetupOrResetForWindows.ps1
+++ SetupOrResetForWindows.ps1
@@ -1,9 +1,10 @@
 # Get the directory where this script is located
 $scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 $bashScriptPath = $scriptDir + "\InitOrResetRoot.sh"
+$wslPath = wsl wslpath "C:\Users\Peter\Projects\file.txt"
 
 try {
-    wsl bash -c "bash '$(wslpath -a -u $bashScriptPath)'"
+    wsl sudo bash "$wslPath"
 } catch {
     Write-Error "Failed to run bash script via WSL: $_"
 }
